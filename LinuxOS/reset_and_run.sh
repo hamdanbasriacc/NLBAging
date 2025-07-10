a06e43edfc09118479d8d47dcf180c7ddd31d165
hamdan.basri <hamdan.basri@accenture.com>
2025-07-10 13:29:31 +0800

    Reset and run updates

--- LinuxOS/reset_and_run.sh
+++ LinuxOS/reset_and_run.sh
@@ -8,10 +8,12 @@ echo "🔍 Checking leftover processes..."
 ps aux | grep watch | grep -v grep
 
 echo "🧹 Cleaning shared input folder..."
-rm -f /home/shared_comfy_data/*.{jpg,jpeg,png}
+#rm -f /home/shared_comfy_data/*.{jpg,jpeg,png}
+rm -f /home/admin/shared_comfy_data/*.{jpg,jpeg,png}
 
 echo "📂 Listing contents of shared input folder..."
-ls -lah /home/shared_comfy_data
+#ls -lah /home/shared_comfy_data
+ls -lah /home/admin/shared_comfy_data
 
 echo "🚀 Restarting ComfyUI watcher..."
 #cd /home/hamdan_basri/ComfyUI/LinuxOS && ./watch_input_and_run.sh
