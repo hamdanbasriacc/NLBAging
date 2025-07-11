8a0a219024cf9aae503fe7364d7610d7983117f5
hamdan.basri <hamdan.basri@accenture.com>
2025-07-11 12:51:03 +0800

    Workflow path update

--- LinuxOS/watch_input_and_run_linux.py
+++ LinuxOS/watch_input_and_run_linux.py
@@ -11,14 +11,15 @@ from watchdog.events import FileSystemEventHandler
 
 # === Config ===
 INPUT_DIR = "/home/admin/shared_comfy_data"
-#INPUT_DIR = "/home/shared_comfy_data"
 OUTPUT_DIR = "/home/admin/ComfyUI/output"
-#OUTPUT_DIR = "/home/hamdan_basri/ComfyUI/output"
-WORKFLOW_PATH = "/home/admin/ComfyUI/user/workflows/aging_workflow.json"
-#WORKFLOW_PATH = "/home/hamdan_basri/ComfyUI/user/workflows/flux_kontext_workflow_flat.json"
-COMFYUI_API_URL = "http://127.0.0.1:8188/prompt"
+WORKFLOW_PATH = "/home/admin/ComfyUI/user/workflows/flux_kontext_workflow_flat.json"
 TARGET_URL_FILE = "/home/admin/shared_comfy_data/latest_aged_url.txt"
+##WORKFLOW_PATH = "/home/admin/ComfyUI/user/workflows/aging_workflow.json"
+#INPUT_DIR = "/home/shared_comfy_data"
+#OUTPUT_DIR = "/home/hamdan_basri/ComfyUI/output"
+#WORKFLOW_PATH = "/home/hamdan_basri/ComfyUI/user/workflows/flux_kontext_workflow_flat.json"
 #TARGET_URL_FILE = "/home/shared_comfy_data/latest_aged_url.txt"
+COMFYUI_API_URL = "http://127.0.0.1:8188/prompt"
 STABILITY_WAIT = 2  # seconds
 
 # === Logging Setup ===
